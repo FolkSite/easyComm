0a2cf60f92adf99023ca12e85763f77731da2124
Alexey Naumov <alexei@createit.ru>
2015-10-07 11:41:11 +0400

    Версия 1.2.4-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,8 +4,8 @@
 define('PKG_NAME', 'easyComm');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '1.2.3');
-define('PKG_RELEASE', 'pl1');
+define('PKG_VERSION', '1.2.4');
+define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 define('PKG_NAMESPACE_PATH', '{core_path}components/' . PKG_NAME_LOWER . '/');
 
